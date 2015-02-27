5cfa93fc2dae820bf5ee07b45f609e24ce542be1
Morgan Curley <morgan.curley@gmail.com>
2015-02-27 08:40:42 -0500

    Updating ruby dependencies

--- server.rb
+++ server.rb
@@ -3,16 +3,29 @@ require 'json'
 require 'git'
 require 'jekyll'
 
+get '/' do
+  'Listening'
+end
+
 post '/' do
 
+    push = JSON.parse(request.body.read)
+    pusher = push["commits"].first["author"]["name"]
+    repo = push["repository"]["url"] + ".git"
+
+    "Relavent Info: " + pusher + "pushed to " + url
+
+end
+
+post '/webhook' do
   dir = './tmp/jekyll'
   name = "JekyllBot"
-  email = "ben+bot@balter.com"
+  email = "morgan.curley+bot@gmail.com"
   username = ENV['GH_USER'] || ''
   password = ENV['GH_PASS'] || ''
-  
+
   FileUtils.rm_rf dir
-  
+
   push = JSON.parse(params[:payload])
   if push["commits"].first["author"]["name"] == name
     puts "This is just the callback from JekyllBot's last commit... aborting."
@@ -21,20 +34,20 @@ post '/' do
 
   url = push["repository"]["url"] + ".git"
   url["https://"] = "https://" + username + ":" + password + "@"
-  
+
   puts "cloning into " + url
-  g = Git.clone(url, dir)  
-   
+  g = Git.clone(url, dir)
+
   options = {}
   options["server"] = false
-  options["auto"] = false 
-  options["safe"] = false 
+  options["auto"] = false
+  options["safe"] = false
   options["source"] = dir
   options["destination"] = File.join( dir, '_site')
   options["plugins"] = File.join( dir, '_plugins')
   options = Jekyll.configuration(options)
   site = Jekyll::Site.new(options)
-  
+
   puts "starting to build in " + dir
   begin
     site.process
@@ -43,11 +56,11 @@ post '/' do
     FileUtils.rm_rf dir
     exit(1)
   end
-  
+
   puts "succesfully built; commiting..."
   begin
     g.config('user.name', name)
-    g.config('user.email', email)  
+    g.config('user.email', email)
     puts g.commit_all( "[JekyllBot] Building JSON files")
   rescue Git::GitExecuteError => e
     puts e.message
@@ -56,10 +69,10 @@ post '/' do
     puts g.push
     puts "pushed"
   end
-  
+
   puts "cleaning up."
   FileUtils.rm_rf dir
-  
+
   puts "done"
-  
+
 end
